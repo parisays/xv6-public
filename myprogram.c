ec7ad2df0c1493e54971c177541488ff93a56a33
parisays <parisa.yalsavar@gmail.com>
2020-01-02 12:28:12 +0330

    Modify myprogram

--- myprogram.c
+++ myprogram.c
@@ -10,20 +10,18 @@ int main (int argc,char *argv[])
 	int status = 20, wait_time, run_time;	
 	pid = fork();
 	if (pid == 0)
-  	{	
-  		// exec(argv[1], argv);
-    	// printf(1, "exec %s failed\n", argv[1]);
-        //argv array for: ls -l
-        char * ls_args[] = { "ls" , "-l", "\0"};
-        //  use the name ls
-        //  rather than the
-        //  path to /bin/ls
-        exec(ls_args[0],ls_args);
+  {	
+      //argv array for: ls -l
+      char * ls_args[] = { "ls" , "-l", "\0"};
+      //  use the name ls
+      //  rather than the
+      //  path to /bin/ls
+      exec(ls_args[0],ls_args);
 
-        //only get here on error
-        printf(1, "exec %s failed\n", ls_args[0]);
-    }
-  	else
+      //only get here on error
+      printf(1, "exec %s failed\n", ls_args[0]);
+  }
+  else
  	{
     	status = waitx(&wait_time, &run_time);
  	}  
