f1b25b0ebfd3ac57bec706698a1932c667011e0c
parisays <parisa.yalsavar@gmail.com>
2020-01-02 18:52:28 +0330

    Add fork to myprogram.c

--- exec.c
+++ exec.c
@@ -99,7 +99,7 @@ exec(char *path, char **argv)
   curproc->sz = sz;
   curproc->tf->eip = elf.entry;  // main
   curproc->tf->esp = sp;
-  curproc->priority = 60; // Q2
+  // curproc->priority = 40; // Q2
   switchuvm(curproc);
   freevm(oldpgdir);
   return 0;
--- myprogram.c
+++ myprogram.c
@@ -4,22 +4,69 @@
 
 int main(int argc, char *argv[])
 {
-  int priority;
+  int priority1, priority2;
 
   if (argc < 2) {
-      printf(2, "Usage: myprogram [priority]\n" );
+      printf(2, "Usage: myprogram [priority1] [priority2]\n" );
       exit();
   }
-  priority = atoi(argv[1]);
+  priority1 = atoi(argv[1]);
+  priority2 = atoi(argv[2]);
 
-  printf(2, "you entered %d\n", priority );
+  // printf(2, "you entered %d\n", priority );
 
-  if (priority < 1 || priority > 100) {
-      printf(2, "Invalid priority (1-100)!\n" );
+  if (priority1 < 1 || priority1 > 100 || priority2 < 1 || priority2 > 100) 
+  {
+      printf(1, "Invalid priority (1-100)!\n" );
       exit();
   }
 
-  setpriority(priority);
+  int pid1;
+	pid1 = fork();
+  if(pid1 < 0)
+  {
+    printf(1, "fork failed\n");
+    exit();
+  }
+	else if (pid1 == 0)
+  {	
+      setpriority(priority1);
+      printf(1,"in child1\n");
+      for(int i=0; i<1000; i++)
+          if(i%3 == 0 && i%7 == 0 && i%5 == 0)
+            printf(1, "GAV\n");
+  }
+  else
+ 	{
+      setpriority(55);
+    	int pid2 = fork();
+      if(pid2 < 0)
+      {
+        printf(1, "fork failed\n");
+        exit();
+      }
+      else if(pid2 == 0)
+      {
+        setpriority(priority2);
+        printf(1,"in child2\n");
+        for(int i=0; i<1000; i++)
+          if(i%3 == 0 && i%7 == 0 && i%5 == 0)
+            printf(1, "KHAR\n");
+        
+        // exit();
+      }
+      else
+      {
+        // setpriority(90);
+        // printf(1,"in parent2\n");
+        // exit();
+      }
+      
+      // printf(1,"in parent1\n");
+      // exit();
+ 	}  
 
+  
+  printf(1, "ghable exit e akhar\n");
   exit();
 }
